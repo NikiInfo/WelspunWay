f08195fdfb8a5bc155822a52c1f8e70670450f9b
shravan2907 <shravan.sv2907@gmail.com>
2023-09-25 16:49:12 +0530

    Commit

--- app/src/main/java/com/example/testapp/HomeActivity.kt
+++ app/src/main/java/com/example/testapp/HomeActivity.kt
@@ -1,12 +1,15 @@
 package com.example.testapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.textclassifier.TextClassifierEvent.TextSelectionEvent.Builder
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.testapp.databinding.ActivityHomeBinding
 import com.example.testapp.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +18,7 @@ import com.google.firebase.ktx.Firebase
 
 
 class HomeActivity : AppCompatActivity() {
+    private val REQUEST_SMS_PERMISSION = 123
     private lateinit var auth: FirebaseAuth
     private lateinit var  binding: ActivityHomeBinding
 
@@ -27,6 +31,7 @@ class HomeActivity : AppCompatActivity() {
             //CODE TO CHANGE STATUS BAR COLOR
 //        val window: Window = this.window
 //        window.statusBarColor = this.resources.getColor(R.color.Welspun)
+
 
         binding.btnProfile.setOnClickListener {
             val intent = Intent(this,UserDetails::class.java)
@@ -45,4 +50,9 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+//    private fun requestSmsPermission(){
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), REQUEST_SMS_PERMISSION)
+//        }
+//    }
 }
